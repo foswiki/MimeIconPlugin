ef84e2a0256b13c90dc6951e0c9f9355208c40c9
MichaelDaum <daum@michaeldaumconsulting.com>
2018-08-16 10:08:14 +0200

    Item14750: disabled spurious message on stderr

--- lib/Foswiki/Plugins/MimeIconPlugin.pm
+++ lib/Foswiki/Plugins/MimeIconPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# MimeIconPlugin is Copyright (C) 2010-2017 Michael Daum http://michaeldaumconsulting.com
+# MimeIconPlugin is Copyright (C) 2010-2018 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -26,8 +26,8 @@ use warnings;
 
 use Foswiki::Func ();
 
-our $VERSION = '2.00';
-our $RELEASE = '30 Aug 2017';
+our $VERSION = '2.01';
+our $RELEASE = '16 Aug 2018';
 our $SHORTDESCRIPTION = 'Icon sets for mimetypes';
 our $NO_PREFS_IN_TOPIC = 1;
 our $baseWeb;
@@ -236,7 +236,7 @@ sub getBestSize {
         last;
       }
     }
-    print STDERR "bestSize=$bestSize size=$size\n";
+    #print STDERR "bestSize=$bestSize size=$size\n";
     return $bestSize;
   }
 }
